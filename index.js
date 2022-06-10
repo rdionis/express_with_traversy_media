694b3246bb8006f7fa9f10516916aeb71d84b828
Raquel Dionísio <dionisio@stadtwerk.org>
2022-06-10 21:16:38 +0200

    adding bad request message if id not existent

--- index.js
+++ index.js
@@ -25,15 +25,23 @@ const app = express()
 
 // get all members
 app.get("/api/members", (req, res) => res.json(members))
+
 //we don't need to do JSON.stringify() even though these are JS objects, because express takes care of that
 
 // get single member
-app.get("api/members/:id", (req, res) => {
-  res.send(req.params.id)
+app.get("/api/members/:id", (req, res) => {
+  // res.send(req.params.id) // just returns the id number
+  const found = members.some(member => member.id === parseInt(req.params.id)) // if it doesn't exist, it will equal false, if it exists, it will equal true
   //res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  //getting a member by their id
+  //id is a URL parameter
+
+  found
+  ? res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  : res.status(400).json({msg: `No member with the ID of ${req.params.id}`})
+
 })
-//getting a member by their id
-//id is a URL parameter
+
 
 
 
